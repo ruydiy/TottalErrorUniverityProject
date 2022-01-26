01854c3bd412721b2427723eeffab8efe85bb5af
ruydiy <radoslavtcankov@abv.bg>
2022-01-26 17:14:41 +0200

    create database

---
+++ Data/TotalErrorDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class TotalErrorDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<ItemType> ItemTypes { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-F8AKO5E9\MYSQLSERVER2017;Database=UniProjectTotalError;Trusted_Connection=True");
+        }
+    }
+}
