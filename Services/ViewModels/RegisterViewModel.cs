f338a072bc50d73a7e320fdc7afc60e4bad12aa8
ruydiy <radoslavtcankov@abv.bg>
2022-01-27 11:49:24 +0200

    add user

---
+++ Services/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ViewModels
+{
+    public class RegisterViewModel
+    {
+        private const string CONFIRM_PASSWORD_STRING = "ConfirmPassword";
+
+        public string Email { get; set; }
+
+        [Compare(CONFIRM_PASSWORD_STRING)]
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
